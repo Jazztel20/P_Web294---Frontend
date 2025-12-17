ba4ee36d002afbce5c8e7c46fe9c3ec61f769cf5
DevStackJunior <pv00wyv@eduvaud.ch>
2025-12-17 10:19:30 +0100

    Feat(Models): Updating reusable components
    Models | Structuring :
    - Book
    - Comment
    - Evaluate

--- Frontend/src/models/Book.ts
+++ Frontend/src/models/Book.ts
@@ -1,25 +1,21 @@
+import type { Writer } from '@/models/Writer'
+import type { Category } from '@/models/Category'
+
 export interface Book {
-    id: string,
-    title: string,
-    numberOfPages: number,
-    pdfLink: string,
-    abstract: string,
-    editor: string,
-    editionYear: Date,
-    imagePath: string,
-    comment: string,
-    categoryId: number,
-    writerId: number,
-    userId: number,
-    global_rating: number,
-    total_comments: number,
-    writer: {
-        id: number,
-        firstname: string,
-        lastname: string
-    },
-    category: {
-        id: number,
-        label: string
-    }
+  id: string
+  title: string
+  numberOfPages: number
+  pdfLink: string
+  abstract: string
+  editor: string
+  editionYear: Date
+  imagePath: string
+  comment: string
+  categoryId: number
+  writerId: number
+  userId: number
+  global_rating: number
+  total_comments: number
+  writer: Writer
+  category: Category
 }
