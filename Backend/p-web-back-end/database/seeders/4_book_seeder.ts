626c8ab8f72d098afd2f39615d62d6b8af44882b
DevStackJunior <pv00wyv@eduvaud.ch>
2025-12-17 10:52:55 +0100

    fix(DB): 1 issue accessibility to writer
    fixing issue accessibility to property writer

--- Backend/p-web-back-end/database/seeders/4_book_seeder.ts
+++ Backend/p-web-back-end/database/seeders/4_book_seeder.ts
@@ -21,7 +21,7 @@ export default class extends BaseSeeder {
         editionYear: 1943,
         imagePath: '/images/petit-prince.jpg',
         categoryId: categories[0].id, // Fiction
-        writerId: writers.find(e => e.firstname == "Antoine"), // Antoine de Saint-Exupéry
+        writerId: writers[1].id,
         userId: users[1].id,
       },
       {
